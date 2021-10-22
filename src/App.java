51d0bbddc694e8e3333c18c9650ac97f5dacb975
Κωνσταντινος Χαφης <koxafis@gmail.com>
2021-10-22 11:07:57 +0300

    .

--- src/App.java
+++ src/App.java
@@ -2,8 +2,7 @@ package src;
 
 public class App{
     public static void main(String[] args) {
-        UI ui = new UI(); // This is temporary until i create gui
-        ui.init();
+        UI.init();
         // City city = new City(5, 2, 3, 5, 6, 1, 7, (float) 52.1, (float) 10.2, (float) 5.1); 
     }
 }
--- src/UI.java
+++ src/UI.java
@@ -1,15 +1,22 @@
 package src;
 
+import java.io.IOException;
+
 public class UI {
     
     UI(){};
 
     public static void init(){
-        City city;
-        while(true){
-            String cityname = Scan.scanString("Type a city's name: ", true);
-            
-            break;
+        try{
+            City city;
+            while(true){
+                String cityname = Scan.scanString("Type a city's name: ", true);
+                String countryInitial = Scan.scanString("And the country's initial (uk, gr etc): ", true);
+                OpenData.RetrieveData(cityname, countryInitial);
+                break;
+            }
+        }catch(IOException err){
+            System.out.println("Error: " + err);
         }
     }
 }
